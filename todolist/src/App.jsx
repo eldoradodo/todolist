4556298af50d49c5cc374d954afb817a7fa544a8
eldoradodo <rla1eh2dus3@naver.com>
2024-05-14 21:54:22 +0900

    second commit

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,59 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const initialState = [
-    { id: 1, name: "John", age: 20 },
-    { id: 2, name: "Doe", age: 21 },
+    { id: 1, title: "John", body: 20 },
+    { id: 2, title: "Doe", body: 21 },
   ];
   const [users, setUsers] = useState(initialState);
 
   // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
+  const [title, settitle] = useState("");
+  const [body, setbody] = useState("");
 
   const addUser = (e) => {
     e.preventDefault();
-    // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
-    if (!name || !age) {
+    // 제목과 나이 모두를 입력 받기.
+    if (!title || !body) {
       alert("이름과 나이를 모두 입력하세요.");
       return;
     }
 
-    // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
-    setUsers([...users, { id: Date.now(), name, age }]);
+    //사용자 리스트 상태를 업데이트. 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용.
+    setUsers([...users, { id: Date.now(), title, body }]);
     // 입력 필드 초기화
-    setName("");
-    setAge("");
+    settitle("");
+    setbody("");
   };
 
   const removeUser = (id) => {
-    // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    // filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
     setUsers(users.filter(user => user.id !== id));
   };
 
   return (
-    <>
-      <h1>사용자 리스트</h1>
+    <div className="container">
+      <h1>TO DO LIST</h1>
       <form onSubmit={addUser}>
         {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="number" placeholder="나이" value={age} onChange={(e) => setAge(e.target.value)} />
-        <button type="submit">사용자 추가</button>
+        <input type="text" placeholder="제목을 입력하세요." value={title} onChange={(e) => settitle(e.target.value)} />
+        <input type="number" placeholder="내용을 입력하세요" value={body} onChange={(e) => setbody(e.target.value)} />
+        <button type="submit">추가하기</button>
       </form>
       <ul>
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
         {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
         {users.map(user => (
           <li key={user.id} style={{ display: 'flex', alignItems: 'center' }}>
-            <span>이름: {user.name}, 나이: {user.age}</span>
+            <span>제목: {user.title}</span>
+            <span> 내용: {user.body}</span>
             <button onClick={() => removeUser(user.id)}>삭제</button>
+            <button onClick={() => removeUser(user.id)}>완료</button>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
