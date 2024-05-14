ba7bfb1a8829ce8be311d4a4465c6e51e61a2f3b
eldoradodo <rla1eh2dus3@naver.com>
2024-05-14 14:52:21 +0900

    first commit

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -4,32 +4,57 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialState = [
+    { id: 1, name: "John", age: 20 },
+    { id: 2, name: "Doe", age: 21 },
+  ];
+  const [users, setUsers] = useState(initialState);
+
+  // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUser = (e) => {
+    e.preventDefault();
+    // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
+    if (!name || !age) {
+      alert("이름과 나이를 모두 입력하세요.");
+      return;
+    }
+
+    // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    setUsers([...users, { id: Date.now(), name, age }]);
+    // 입력 필드 초기화
+    setName("");
+    setAge("");
+  };
+
+  const removeUser = (id) => {
+    // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    setUsers(users.filter(user => user.id !== id));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>사용자 리스트</h1>
+      <form onSubmit={addUser}>
+        {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
+        <input type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="number" placeholder="나이" value={age} onChange={(e) => setAge(e.target.value)} />
+        <button type="submit">사용자 추가</button>
+      </form>
+      <ul>
+        {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
+        {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map(user => (
+          <li key={user.id} style={{ display: 'flex', alignItems: 'center' }}>
+            <span>이름: {user.name}, 나이: {user.age}</span>
+            <button onClick={() => removeUser(user.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
